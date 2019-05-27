cc32c904941f40ef6a132e2bf39b8a95090b3116
Jesusmaster <armandodejesus678@gmail.com>
2019-05-27 17:08:17 -0500

    agregando redux en el servidor

--- source/server.js
+++ source/server.js
@@ -4,6 +4,11 @@ import { StaticRouter } from 'react-router';
 import { renderToStaticMarkup, renderToString } from 'react-dom/server'
 import Markup from './Markup';
 import App from './app/app';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import reducer from './app/reducer.js';
+
 
 const app = express();
 
@@ -12,11 +17,20 @@ app.use('/public', express.static('./public'));
 
 
 app.get('*', function (request, response){
+
+  //create store
+  const store = createStore(
+    reducer,
+    applyMiddleware(ReduxThunk)
+  );
+
   const context = {};
   const content=renderToString(
-    <StaticRouter context={context} location={request.url}>
-      <App />
-    </StaticRouter>
+    <Provider store={store}>
+      <StaticRouter context={context} location={request.url}>
+        <App />
+      </StaticRouter>
+    </Provider>
   );
 
   const html = renderToStaticMarkup(<Markup content={content} />);
