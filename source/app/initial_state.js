f1e70c673fa645b606129ca22980ca0100591a18
Jesusmaster <armandodejesus678@gmail.com>
2019-05-27 16:23:35 -0500

    agregando cliente api para hacer fetchs

---
+++ source/app/initial_state.js
@@ -0,0 +1,14 @@
+const initial_state = {
+  pages: {
+    list: {
+      loading: false,
+      items: [],
+    },
+    view: {
+      loading: false,
+      data: null,
+    }
+  }
+}
+
+export default initial_state;
