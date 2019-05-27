d66625d80409e9684caf8d2031070f75f475d92a
Jesusmaster <armandodejesus678@gmail.com>
2019-05-27 12:37:30 -0500

    comit inicial, instalacion de dependencias y archivos de configuracion webpack

---
+++ webpack/webpack.client.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const extract = require('mini-css-extract-plugin')
+
+const config = {
+  entry: ["@babel/polyfill", path.resolve(__dirname, '../source/client.js')],
+  output: {
+    path: path.resolve(__dirname, '../public'),
+    filename: 'app.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: "/(node_modules)/",
+        options: {
+          presets: ['@babel/preset-env','@babel/preset-react'],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [extract.loader, "css-loader?modules=true&localIdentName=[hash:base64:15]"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.css']
+  },
+  plugins: [
+    new extract({
+      filename: "style.css"
+    })
+  ],
+  target: 'web',
+}
+
+module.exports = config;
---
+++ webpack/webpack.server.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const extract = require('mini-css-extract-plugin')
+
+const config = {
+  entry: ["@babel/polyfill", path.resolve(__dirname, '../source/server/server.js')],
+  output: {
+    path: path.resolve(__dirname, '../build'),
+    filename: 'server.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: "/(node_modules)/",
+        options: {
+          presets: ['@babel/preset-env','@babel/preset-react'],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [extract.loader, "css-loader?modules=true&localIdentName=[hash:base64:15]"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.css']
+  },
+  plugins: [
+    new extract({
+      filename: "style.css"
+    })
+  ],
+  target: 'node',
+}
+
+module.exports = config;
